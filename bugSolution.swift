565bdf56c4fc73288d1d08a7428e281efb7bc378
BugHunter-X <askzenai@gmail.com>
2025-02-20 21:04:05 +0100

    Add bugSolution.swift file

---
+++ bugSolution.swift
@@ -0,0 +1,9 @@
+func calculateArea(width: Double, height: Double) -> Double {
+    return width * height
+}
+
+let area = calculateArea(width: 10, height: 5) // Correct usage
+
+//Corrected usage:
+let correctArea = calculateArea(width: 10, height: 5) //Correct usage with labels
+print(correctArea) // Output: 50.0
